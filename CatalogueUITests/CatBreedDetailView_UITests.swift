9a873d25aef5957972a09bbe484f61f7d9e22fe8
NathanRUbeda <ubedanathan@gmail.com>
2025-02-13 17:21:51 -0500

    Updated test to fix performance and handle secret API key.

--- CatalogueUITests/CatBreedDetailView_UITests.swift
+++ CatalogueUITests/CatBreedDetailView_UITests.swift
@@ -14,6 +14,7 @@ final class CatBreedDetailView_UITests: XCTestCase {
     override func setUpWithError() throws {
         continueAfterFailure = false
 		let app = XCUIApplication()
+		app.launchEnvironment = ["IS_TEST": "TRUE"]
 		app.launch()
     }
 
@@ -21,31 +22,27 @@ final class CatBreedDetailView_UITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
 		app = nil
     }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
 	
 	func test_CatBreedDetailView_favoriteButton_shouldToggleFavorited() throws {
 		// Given
 		guard let app else { return }
-		app.scrollViews.otherElements.buttons["Abyssinian, The Abyssinian is easy to care for, and a joy to have in your home. They’re affectionate cats and love both people and other animals."].tap()
-		let abyssinianNavigationBar = app.navigationBars["Abyssinian"]
 		
 		// When
-		abyssinianNavigationBar/*@START_MENU_TOKEN@*/.buttons["heart"]/*[[".otherElements[\"Love\"]",".buttons[\"Love\"]",".buttons[\"heart\"]",".otherElements[\"heart\"]"],[[[-1,2],[-1,1],[-1,3,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.tap()
+		app.scrollViews.otherElements/*@START_MENU_TOKEN@*/.buttons["CatBreedAmerican CurlButton"]/*[[".buttons[\"American Curl, Distinguished by truly unique ears that curl back in a graceful arc, offering an alert, perky, happily surprised expression, they cause people to break out into a big smile when viewing their first Curl. Curls are very people-oriented, faithful, affectionate soulmates, adjusting remarkably fast to other pets, children, and new situations.\"]",".buttons[\"CatBreedAmerican CurlButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
 		
+		let americanCurlNavigationBar = app.navigationBars["American Curl"]
+		americanCurlNavigationBar/*@START_MENU_TOKEN@*/.buttons["heart"]/*[[".otherElements[\"Love\"]",".buttons[\"Love\"]",".buttons[\"heart\"]",".otherElements[\"heart\"]"],[[[-1,2],[-1,1],[-1,3,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.tap()
+						
 		// Then
+		XCTAssert(americanCurlNavigationBar/*@START_MENU_TOKEN@*/.buttons["heart.fill"]/*[[".otherElements[\"Love\"]",".buttons[\"Love\"]",".buttons[\"heart.fill\"]",".otherElements[\"heart.fill\"]"],[[[-1,2],[-1,1],[-1,3,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.exists)
 	}
 	
 	func test_CatBreedDetailView_backButton_shouldReturnToPreviousView() throws {
 		// Given
-		guard let app else { return }
+		guard let app else {
+			return
+		}
+		
 		let catBreedNavigationLink = app.scrollViews.otherElements.buttons["Abyssinian, The Abyssinian is easy to care for, and a joy to have in your home. They’re affectionate cats and love both people and other animals."]
 		
 		
