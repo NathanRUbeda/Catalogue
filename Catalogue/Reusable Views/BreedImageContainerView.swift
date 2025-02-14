8899db37f8f6fa0de3e406765b5f93cb07b4521e
NathanRUbeda <ubedanathan@gmail.com>
2025-02-13 23:56:05 -0500

    + BreedImageContainerView to store and customize image.

--- Catalogue/Reusable Views/BreedImageContainerView.swift
+++ Catalogue/Reusable Views/BreedImageContainerView.swift
@@ -7,12 +7,29 @@
 
 import SwiftUI
 
-struct BreedImageContainerView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-#Preview {
-    BreedImageContainerView()
+/// Displays a customizable cat breed image.
+struct BreedImageContainerView<ImageView: View>: View {
+	// MARK: Injected properties
+	/// The width of the image.
+	let width: CGFloat
+	
+	/// The height of the image.
+	let height: CGFloat
+	
+	/// The corner radius of the image.
+	let cornerRadius: CGFloat
+	
+	/// The cat breed image view to display.
+	let imageView: ImageView
+	
+	var body: some View {
+		self.imageView
+			.frame(width: self.width, height: self.height)
+			.clipped()
+			.clipShape(.rect(cornerRadius: self.cornerRadius))
+			.overlay(
+				RoundedRectangle(cornerRadius: self.cornerRadius)
+					.stroke(.gray, lineWidth: 1)
+			)
+	}
 }
